7865a63000e9342bfe586fd2c036be3cf7f180e7
Abdalla Elshikh <43406571+abdullahsalah96@users.noreply.github.com>
2020-04-19 07:58:58 +0200

    cropping image

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -15,9 +15,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var topField: UITextField!
     @IBOutlet weak var bottomField: UITextField!
-    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var activityButton: UIButton!
+    //text field delegaet
     let textFieldDelegate = TextFieldDelegate()
+    //variable used for shifting keyboard
     var keyboardShift:CGFloat = 0
+    //struct for holding meme params
+    struct Meme {
+        var topText:String?
+        var bottoomText:String?
+        var originalImage:UIImage?
+        var memedImage:UIImage?
+    }
     
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
@@ -49,12 +59,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
     func configureUI(){
+        setActivityButtonsState(false)
         topField.borderStyle = .none
         topField.text = "TOP"
         topField.textColor = UIColor.white
         topField.defaultTextAttributes = memeTextAttributes
         topField.delegate = textFieldDelegate
-        
         bottomField.borderStyle = .none
         bottomField.text = "BOTTOM"
         bottomField.textColor = UIColor.white
@@ -69,12 +79,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
+        setActivityButtonsState(true)
     }
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
+        setActivityButtonsState(true)
+    }
+    
+    func setActivityButtonsState(_ x:Bool){
+        cancelButton.isEnabled = x
+        activityButton.isEnabled = x
     }
     
     @objc func keyboardWillShow(_ notification:Notification) {
@@ -94,9 +111,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.keyboardShift = 0
         }
     }
-
+    
     func getKeyboardHeight(_ notification:Notification) -> CGFloat {
-
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
         return keyboardSize.cgRectValue.height
@@ -108,8 +124,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     func unsubscribeToKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @IBAction func shareMeme(_ sender: Any) {
+        
+        //activity view controller
+        let img = generateMemedImage()
+        let viewController = UIActivityViewController(activityItems: [img], applicationActivities: nil)
+        present(viewController, animated: true, completion: nil)
+    }
+    
+    func save(){
+        // Create the meme
+        let memedImage = generateMemedImage()
+        let meme = Meme(topText: topField.text!, bottoomText: bottomField.text!, originalImage: imagePickerView.image, memedImage: memedImage)
+    }
+    
+    func generateMemedImage() -> UIImage {
+        // Render view to an image
+
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        //make a cropping rectangle
+        let croppingRect = CGRect(x: 30, y: 110, width: memedImage.size.width - CGFloat(80), height: memedImage.size.height - CGFloat(250))
+        //crop image based on rectangle
+        let croppedImage = memedImage.cgImage!.cropping(to: croppingRect)
+        UIGraphicsEndImageContext()
+        return UIImage(cgImage: croppedImage!)
     }
     
   
